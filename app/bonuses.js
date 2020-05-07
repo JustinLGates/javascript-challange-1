61d3074281b2bebc7f1c3fbfb333d7c28dbb1e8c
Justin Gates <papagates1@gmail.com>
2020-05-07 08:26:47 -0600

    did all challanges

--- app/bonuses.js
+++ app/bonuses.js
@@ -1,6 +1,24 @@
 //Given the string "team number 2", transform and return the string in camelCase
-let str = "team number 2"
+let str = "team number 2";
 
 function camelCase(str) {
-
+  // console.log(`original string: ${str}`);
+  let words = str.split(" ");
+  let result = [words[0]];
+  // console.log(result);
+  for (let i = 1; i < words.length; i++) {
+    const word = words[i];
+    result.push(word[0].toUpperCase() + word.slice(1));
+  }
+  console.log("final result: " + result.join(""));
+  return result.join("");
 }
+
+// let words = str.split(" ");
+// let result;
+// words.forEach((word) => {
+//   isNaN(word)
+//     ? (result += word.split("")[0].toUpperCase() + word.slice(1))
+//     : (result += word);
+// });
+// return result;
--- app/conditionals.js
+++ app/conditionals.js
@@ -1,12 +1,15 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
-
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
-// Example: 
+// Example:
 // input: 23, 25
 // output: "A"
 // Example:
@@ -14,12 +17,23 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+  let ave = (score / possible) * 100;
+  let letter;
+  if (ave >= 90) {
+    letter = "A";
+  } else if (ave >= 80) {
+    letter = "B";
+  } else if (ave >= 70) {
+    letter = "C";
+  } else if (ave >= 60) {
+    letter = "D";
+  } else {
+    letter = "F";
+  }
+  return letter;
 }
 
-
 // --------------------------------------------
-
 
 // 3. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
 //Morning is between 5am and 11am (5 - 11)
@@ -29,34 +43,47 @@ function testGrader(score, possible) {
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
-    // you code below
-
+  if (hour >= 5 && hour <= 11) {
+    return "good morning";
+  } else if (hour >= 12 && hour <= 17) {
+    return "good afternoon";
+  } else if (hour >= 18 && hour <= 21) {
+    return "good evening";
+  } else if ((hour >= 22 && hour <= 24) || hour <= 4) {
+    return "good night";
+  }
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+  if (temp <= 98.6) {
+    return "no fever";
+  } else if (temp < 103) {
+    return "fever";
+  } else if (temp >= 103) {
+    return "fever go to hospital";
+  }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
 let myCar = {
-    make: "Ford",
-    model: "Mustang",
-    color: "Red",
-    moving: false
-}
+  make: "Ford",
+  model: "Mustang",
+  color: "Red",
+  moving: false,
+};
 
 function isStopped(car) {
-
+  return !car.moving;
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
 
 let dish = {
-    yourDish: true,
-    isDirty: true
-}
+  yourDish: true,
+  isDirty: true,
+};
 
 function washDish(dish) {
-
+  return dish.yourDish && dish.isDirty;
 }
