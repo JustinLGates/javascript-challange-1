5375bfbdadfb9aa7efe98f81c515057a58d57d32
Justin Gates <papagates1@gmail.com>
2020-05-07 08:27:28 -0600

    minor change removed log msgs

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,15 +2,12 @@
 let str = "team number 2";
 
 function camelCase(str) {
-  // console.log(`original string: ${str}`);
   let words = str.split(" ");
   let result = [words[0]];
-  // console.log(result);
   for (let i = 1; i < words.length; i++) {
     const word = words[i];
     result.push(word[0].toUpperCase() + word.slice(1));
   }
-  console.log("final result: " + result.join(""));
   return result.join("");
 }
 
